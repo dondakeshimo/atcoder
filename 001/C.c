219969f0dda5cb0ccd1cbd5329fbc97d0528c099
dondakeshimo <went.went.takkun135@gmail.com>
2018-06-19 01:42:06 +0900

    [add]c問題を途中までやったけどif分やったからやめた

---
+++ 001/C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int deg, dis;
+    int a_deg, a_dis
+
+    scanf("%d %d", &deg, &dis);
+
+    if (1125 <= deg && deg < 3375) {
+        a_deg = 
+    }
+}
