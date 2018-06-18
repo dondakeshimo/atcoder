d0767f91faaa9bf9715a46d3fdeacf4269e54dba
dondakeshimo <went.went.takkun135@gmail.com>
2018-06-19 01:33:25 +0900

    [add]001のB問題を追加

---
+++ 001/B.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int m;
+    int vv;
+
+    scanf("%d", &m);
+
+    if (m < 100) {
+        vv = 0;
+    } else if (100 <= m && m <= 5000) {
+        vv = m / 100;
+    } else if (6000 <= m && m <= 30000) {
+        vv = m / 1000 + 50;
+    } else if (35000 <= m && m <= 70000) {
+        vv = (m / 1000 - 30) / 5 + 80;
+    } else if (70000 < m) {
+        vv = 89;
+    }
+
+    printf("%02d\n", vv);
+    return 0;
+}
